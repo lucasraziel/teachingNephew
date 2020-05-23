36379ee9e528486af72637cf68b091d655c496b7
Lucas <lucas.raziel@gmail.com>
2020-05-22 21:24:35 -0300

    exercise to nephew

---
+++ program.js
@@ -0,0 +1,33 @@
+
+//retorne a área de um retângulo
+//lado a e b
+function areaRectangle (a,b){
+
+  return x
+}
+
+
+console.log(areaRectangle(3,4))
+
+//retorne a área de um circulo
+// raio
+
+function areaCircle (r){
+
+  return x
+}
+
+//retorne a área de um quadrado
+
+console.log(areaCircle(3))
+
+
+function areaSquare(l){
+
+  return x
+}
+
+console.log(areaSquare(2))
+
+
+
